be4a22cd2a13bfc84849495fe1949e9194f142bc
Udit Kumar Swaroopa <46641098+UditKuma@users.noreply.github.com>
2019-09-02 22:08:38 +0530

    Add files via upload

---
+++ theme.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class MyTheme {
+  static Color darkblue = Color(0xff161468);
+  static Color blue = Color(0xff1a64c4);
+  static Color green = Color(0xff1cef5a);
+  static Color orange = Color(0xff1cef5a);
+}
